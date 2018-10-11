5b0c6a1230790c953d9fc8f77bac632db2d21409
Daniel Evans <danevans@terpmail.umd.edu>
2018-10-11 18:07:19 -0400

    script cache

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser')
 const store = require('./store')
 var path    = require("path");
 const app = express()
-app.use(express.static('/public'))
+app.use(express.static('public'))
 app.use(bodyParser.json())
+
 app.post('/createUser', (req, res) => {
   store
     .createUser({
