4d16c9b15b816e2e0de7681532769d35300df9c9
Daniel Evans <danevans@terpmail.umd.edu>
2018-12-11 12:00:31 -0500

    mmmm

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+var bodyParser = require('body-parser')
 const store = require('./store')
 var path    = require("path");
 var nodemailer = require('nodemailer');
@@ -8,10 +8,10 @@ var alert = require('alert-node')
 const knex = require('knex')(require('./knexfile'))
 
 var con = mysql.createConnection({
-  host: 'ls-d5e856bd4792d1c8400c321d9e6e0c10c3ffc9e8.c0o4lddlobrx.us-east-1.rds.amazonaws.com',
-  user: 'dbmasteruser',
-  password: 'password',
-  database: 'Project'
+	host: 'ls-d5e856bd4792d1c8400c321d9e6e0c10c3ffc9e8.c0o4lddlobrx.us-east-1.rds.amazonaws.com',
+	user: 'dbmasteruser',
+	password: 'password',
+	database: 'Project'
 });
 
 var ejs = require('ejs')
@@ -26,45 +26,48 @@ var code;
 var id;
 var categorylist = [];
 
+
+
 app.use(express.static('public'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+app.use(bodyParser.urlencoded({
+    extended: true,
+    limit: '200mb'
+}));
 
 app.post('/createUser', (req, res) => {
-  var transporter = nodemailer.createTransport({
-  	service: 'gmail',
-  	auth: {
-  		user: '424EmailSender@gmail.com',
-  		pass: 'databases'
-  	}
-  });
+	var transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: '424EmailSender@gmail.com',
+			pass: 'databases'
+		}
+	});
 
-  code = Math.floor(Math.random() * Math.floor(9999));
+	code = Math.floor(Math.random() * Math.floor(9999));
 
-  var mailOptions = {
-  	from: '424EmailSender@gmail.com',
-  	to: req.body.email,
-  	subject: 'Verification Email',
-  	text: code.toString()
-  };
+	var mailOptions = {
+		from: '424EmailSender@gmail.com',
+		to: req.body.email,
+		subject: 'Verification Email',
+		text: code.toString()
+	};
 
-  console.log(req.body)
+	console.log(req.body)
 
-  transporter.sendMail(mailOptions,function(error,info){
-  	if(error){
-  	  console.log(error);
-  	} else {
-  	  console.log('Email sent: ' + info.response)
-  	  name = req.body.name
-	  email = req.body.email
-	  password = req.body.password
-	  publisher =  req.body.publisher
-	  res.sendFile(path.join(__dirname + '/createUser.html'))
+	transporter.sendMail(mailOptions,function(error,info){
+		if(error){
+			console.log(error);
+		} else {
+			console.log('Email sent: ' + info.response)
+			name = req.body.name
+			email = req.body.email
+			password = req.body.password
+			publisher =  req.body.publisher
+			res.sendFile(path.join(__dirname + '/createUser.html'))
 
-    }  
-  	
+		}  
+
 	})
 })
 
@@ -76,48 +79,48 @@ app.post('/login', (req,res) => {
 	date = d.toString().split('T')[0].split('-');
 	datenumber = date[0] + date[1] + date[2];
 	console.log(datenumber)
-	var query = "SELECT MessageID from message_table where DATEDIFF("
+	var query = "SELECT MessageID from message_table where DATEDIFF(";
 
 
 
-var query = "SELECT CategoryName from categories"
-con.query(query, function(err,result,fields) {
-	if(err) throw err;
-	if(result === undefined || result.length == 0){
-		categorylist = [];
-	} else {
-		result.forEach(function(r) {
-			categorylist.push(r['CategoryName']);
-		});
-    console.log(categorylist)
-		
-	}
-})
+	var query = "SELECT CategoryName from categories"
+	con.query(query, function(err,result,fields) {
+		if(err) throw err;
+		if(result === undefined || result.length == 0){
+			categorylist = [];
+		} else {
+			result.forEach(function(r) {
+				categorylist.push(r['CategoryName']);
+			});
+			console.log(categorylist)
+			
+		}
+	})
 
 
-//used to login by checking database
-	
-var query = "SELECT publisher, id FROM user where email = " + mysql.escape(req.body.email) + " AND password = " + mysql.escape(req.body.password);
-    
-    con.query(query, function(err,result,fields) {
-      if (err) throw err;
-      if(result === undefined || result.length == 0){
-      	alert('Invalid email or password.')
-      	res.sendFile(__dirname + '/index.html')     
-      }
-      else if(result[0]['publisher'] == 0){
-        console.log(categorylist)
-        res.render('reader.ejs', {categories: categorylist})
-      } else if (result[0]['publisher'] == 1){
-        email = req.body.email
-        password = req.body.password
-        id = result[0]['id']
-        console.log(id)
-        res.render('publisher.ejs')
-      } 
-    })
+	//used to login by checking database
 
-	
+	var query = "SELECT publisher, id FROM user where email = " + mysql.escape(req.body.email) + " AND password = " + mysql.escape(req.body.password);
+
+	con.query(query, function(err,result,fields) {
+		if (err) throw err;
+		if(result === undefined || result.length == 0){
+			alert('Invalid email or password.')
+			res.sendFile(__dirname + '/index.html')     
+		}
+		else if(result[0]['publisher'] == 0){
+			console.log(categorylist)
+			res.render('reader.ejs', {categories: categorylist})
+		} else if (result[0]['publisher'] == 1){
+			email = req.body.email
+			password = req.body.password
+			id = result[0]['id']
+			console.log(id)
+			res.render('publisher.ejs')
+		} 
+	})
+
+
 
 	//testing purpose to login as reader
 	/*res.render('reader.ejs', {
@@ -138,28 +141,28 @@ var query = "SELECT publisher, id FROM user where email = " + mysql.escape(req.b
 
 
 app.post('/verifyUser', (req,res) => {
-  console.log(req.body)
-  if(req.body.entered == code.toString()){
-  	  store
-    .createUser({
-      name: name,
-      email: email,
-      password: password,
-      publisher: publisher
-    })
-    .then(() => res.sendStatus(200))
-    res.sendFile(path.join(__dirname + '/index.html'))
-  } else {
-  	console.log('incorrect code')
-  }
+	console.log(req.body)
+	if(req.body.entered == code.toString()){
+		store
+		.createUser({
+			name: name,
+			email: email,
+			password: password,
+			publisher: publisher
+		})
+		.then(() => res.sendStatus(200))
+		res.sendFile(path.join(__dirname + '/index.html'))
+	} else {
+		console.log('incorrect code')
+	}
 })
 
 app.post('/queryUser', (req, res) => {
 	store
-		.queryUser({
-			name: req.body.name
-		})
-  res.sendFile(path.join(__dirname+'/index.html'));
+	.queryUser({
+		name: req.body.name
+	})
+	res.sendFile(path.join(__dirname+'/index.html'));
 })
 
 
@@ -167,67 +170,66 @@ app.post('/queryUser', (req, res) => {
 
 app.post('/postStory', (req, res) => {
 	console.log('post Story')
-  console.log(id)
-  console.log(reader.readAsDataURL(req.body.pic));
-
+	console.log(id)
+	console.log(req.body.b64pic);
 	store
-		.postStory({
-			Email: email,
-			Password: password,
-      UserID: id,
-			Content: req.body.comment,
-			StartTime: req.body.starttime,
-			EndTime: req.body.endtime,
-			Latitude: req.body.latitude,
-      Longitude: req.body.longitude,
-			Range: req.body.range,
-			Categories: req.body.category,
-      Media: reader.readAsDataURL(req.body.pic)
-		})
-    .then(() => res.sendStatus(200))
-    res.render('publisher.ejs');
+	.postStory({
+		Email: email,
+		Password: password,
+		UserID: id,
+		Content: req.body.comment,
+		StartTime: req.body.starttime,
+		EndTime: req.body.endtime,
+		Latitude: req.body.latitude,
+		Longitude: req.body.longitude,
+		Range: req.body.range,
+		Categories: req.body.category,
+		Media: req.body.b64pic
+	})
+	.then(() => res.sendStatus(200))
+	res.render('publisher.ejs');
 })
 
 app.post('/searchStories', (req, res) => {
-  var query = "SELECT U.name, M.Content, M.Categories, M.Media FROM message_table M, user U \
-                 WHERE id = UserID AND M.Categories = " + mysql.escape(req.body.category) + "\
-                 OR M.range > SQRT(POW(M.Latitude - " + 69*req.body.lat + ", 2) + \
-                 POW(M.Longitude - " + 69*req.body.long + ", 2))";
-  con.query(query, function(err,result,fields) {
-    if(result === undefined || result.length == 0){
-      console.log("empty")
-      res.render('publisher.ejs');
-    }
-    else {
-      posts = [];
-      result.forEach(function(r){
-      console.log(r)
-        posts.push({name: r['name'],
-         category: r['Categories'],
-         media: "data:image/jpg;base64," + r['Media'],
-         description: r['Content']
-        })       
-      });
-      res.render('publisher.ejs', posts)
-    }
-  })
+	var query = "SELECT U.name, M.Content, M.Categories, M.Media FROM message_table M \
+	INNER JOIN user U ON U.id = M.UserID WHERE M.Categories LIKE \"%{" + mysql.escape(req.body.category) + "}%\"\
+	AND M.range > SQRT(POW(M.Latitude * 69 - " + 69*req.body.lat + ", 2) + \
+	POW(M.Longitude * 69 - " + 69*req.body.long + ", 2))";
+	con.query(query, function(err,result,fields) {
+		if(result === undefined || result.length == 0){
+			posts = [];
+			console.log("empty")
+			res.render('publisher.ejs');
+		}
+		else {
+			posts = [];
+			result.forEach(function(r){
+				posts.push({name: r['name'],
+					category: r['Categories'],
+					media: r['Media'],
+					description: r['Content']
+				})       
+			});
+			res.render('publisher.ejs', posts)
+		}
+	})
   /*store
     .searchStories({
       Category: req.body.category,
       Latitude: req.body.lat,
       Longitude: req.body.long
-    })*/
+  })*/
 	//res.render('publisher.ejs', {posts: [req.body.category]})
 
 })
 
 app.post('/addCategory', (req, res) => {
-  store.addCategory({
-    CategoryName: req.body.categoryname,
-    Parent: req.body.parentcategory
-  })
-  .then(() => res.sendStatus(200))
-  res.render('publisher.ejs');
+	store.addCategory({
+		CategoryName: req.body.categoryname,
+		Parent: req.body.parentcategory
+	})
+	.then(() => res.sendStatus(200))
+	res.render('publisher.ejs');
 })
 
 // Reader end points
@@ -242,10 +244,10 @@ app.post('/findStories', (req, res) =>{
 	var cats = req.body.categories.toString().split(',');
 
 	var query = "SELECT U.name, M.Content, M.Categories FROM message_table M, user U \
-                 WHERE M.range > SQRT(POW(M.Latitude - " + 69*req.body.lat + ", 2) + \
-                 POW(M.Longitude - " + 69*req.body.long + ", 2))";
+	WHERE M.range > SQRT(POW(M.Latitude - " + 69*req.body.lat + ", 2) + \
+	POW(M.Longitude - " + 69*req.body.long + ", 2))";
 
-    for (var i = cats.length - 1; i >= 0; i--) {
+	for (var i = cats.length - 1; i >= 0; i--) {
 		if (i == cats.length-1) {
 			query += " AND (message_table.Categories LIKE \"%{" + cats[i] + "}%\""
 		} else {
@@ -257,18 +259,33 @@ app.post('/findStories', (req, res) =>{
 
 	}
 
-    console.log(query);
-
-	res.render('reader.ejs', {posts: [req.body], categories: categorylist})
+	con.query(query, function(err,result,fields) {
+		if(result === undefined || result.length == 0){
+			posts = [];
+			console.log("empty")
+			res.render('reader.ejs', {categories: categorylist});
+		}
+		else {
+			posts = [];
+			result.forEach(function(r){
+				posts.push({name: r['name'],
+					category: r['Categories'],
+					media: r['Media'],
+					description: r['Content']
+				})       
+			});
+			res.render('reader.ejs', {posts: posts, categories: categorylist})
+		}
+	})
 })
 
 
 
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+	res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(7555, () => {
-  console.log('Server running on http://localhost:7555')
+	console.log('Server running on http://localhost:7555')
 })
